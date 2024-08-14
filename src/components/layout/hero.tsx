9c7088e5ca3ae0d620ba68816c41b28d22e2384f
unknown <you@example.com>
2024-08-14 21:33:33 +0800

    adjust margins

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FlexBox } from "../Flexbox";
 import Image from "next/image";
-import { TypeAnimation } from "react-type-animation";
 import Button from "../Button";
 import { TextEffect } from "../TextEffect";
 import { InView } from "../InView";
@@ -22,17 +21,17 @@ const Hero = ({ ...props }: HeroProps) => {
             x: 0,
           },
         }}
-        transition={{ duration: .9, ease: "easeInOut" }}
+        transition={{ duration: 0.9, ease: "easeInOut" }}
         viewOptions={{ margin: "0px 0px -350px 0px" }}
       >
-        <FlexBox className="md:mt-14 mt-10 flex flex-col lg:flex-row gap-16 lg:gap-0 lg:min-h-[calc(100vh-80px)] pt-14 md:pt-[2rem] lg:pt-[8rem] w-full relative">
+        <FlexBox className="md:mt-14 mt-10 flex flex-col lg:flex-row gap-16 lg:gap-0 lg:min-h-[calc(100vh-80px)] pt-[6rem] md:pt-[2rem] lg:pt-[8rem] w-full relative">
           <FlexBox className="flex-1 sm:mt-5 gap-2" flexDirection="col">
             <h5 className="font-poppins text-base text-foreground/85">
               Hi there &#128075;, I'm
             </h5>
-            <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold font-poppins tracking-tight text-foreground/85">
+            <h1 className="text-4xl font-extrabold font-poppins tracking-tight text-foreground/85">
               Rex Randy, an{" "}
-              <span className="bg-clip-text text-transparent bg-gradient-to-tr from-primaryAccent to-primary">
+              <span className="bg-clip-text text-transparent bg-gradient-to-tr z-10 from-primaryAccent to-primary relative after:absolute after:left-0 after:bottom-[8px] after:z-0 after:rounded-sm after:bg-gradient-to-r after:from-primaryAccent after:to-primary after:h-[4px] after:w-full">
                 aspiring&nbsp;
               </span>
               Fullstack Developer.
@@ -54,6 +53,7 @@ const Hero = ({ ...props }: HeroProps) => {
           </FlexBox>
           <FlexBox className="flex-1">
             <Image
+             className="scale-110 md:scale-105 lg:scale-100"
               width={800}
               height={700}
               alt="heroimage"
