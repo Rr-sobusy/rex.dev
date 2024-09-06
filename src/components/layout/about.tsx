7d02b4bd412ee7bb195a2f7038ffb8edda43da56
unknown <rockph0029@gmail.com>
2024-09-06 18:58:41 +0800

    add marque component

--- src/components/layout/about.tsx
+++ src/components/layout/about.tsx
@@ -6,10 +6,86 @@ import { FlexBox } from "../Flexbox";
 import Image from "next/image";
 import MyImage from "@/../public/rex_image.png";
 import { BorderBeam } from "../BorderBeam";
+import { Marquee } from "../Marque";
+import { Logos } from "@/constants/Marque-Logos";
+import { cn } from "@/lib/utils";
 
 type Props = {};
-
+const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://avatar.vercel.sh/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://avatar.vercel.sh/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/john",
+  },
+  {
+    name: "Jane",
+    username: "@jane",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jane",
+  },
+  {
+    name: "Jenny",
+    username: "@jenny",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jenny",
+  },
+  {
+    name: "James",
+    username: "@james",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/james",
+  },
+];
+function ReviewCard({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) {
+  return (
+    <figure
+      className={cn(
+        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        // light styles
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        // dark styles
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium dark:text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium dark:text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+}
+const firstRow = reviews.slice(0, reviews.length / 2);
 const About = (props: Props) => {
+  const arr = [Logos.tailwindcss, Logos.framer, Logos.nextjs, Logos.aws];
   return (
     <section id="About" className="mt-8 min-h-screen">
       <h1 className="font-poppins text-xl font-extrabold tracking-wide text-foreground/85 w-full transition-[ease] duration-[1]">
@@ -39,15 +115,23 @@ const About = (props: Props) => {
           </p>
           <p className="font-poppins text-foreground/85 mt-3">
             Hi my name is Rex Randy Hernandez. An aspiring{" "}
-            <strong>fullstack developer.</strong>I'm driven by a desire to
-            create innovative solutions that make a positive impact especially
-            to businesses. I'm a collaborative team player who thrives on
-            solving complex problems and continuously learning new technologies.
-            My goal is to contribute my expertise to a dynamic team and help
-            build exceptional products.
+            <strong>fullstack developer.</strong> I'm driven by a desire to
+            create <strong>innovative solutions</strong> that make a positive
+            impact especially to businesses. I'm a collaborative{" "}
+            <strong>team player</strong> who thrives on solving complex problems
+            and continuously learning new technologies. My goal is to contribute
+            my expertise to a dynamic team and help them to build exceptional
+            products.
           </p>
         </FlexBox>
       </FlexBox>
+      <div>
+        <Marquee pauseOnHover className="[--duration:15s]">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+      </div>
     </section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,18 +9,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        boxesBackground: `linear-gradient(45deg, transparent 24%, #ECECEC 25%, #ECECEC 26%, transparent 27%),
-                          linear-gradient(315deg, transparent 24%, #ECECEC 25%, #ECECEC 26%, transparent 27%),
-                          linear-gradient(225deg, transparent 24%, #ECECEC 25%, #ECECEC 26%, transparent 27%),
-                          linear-gradient(135deg, transparent 24%, #ECECEC 25%, #ECECEC 26%, transparent 27%)`,
-      },
-      backgroundSize: {
-        boxesSize: "55px 55px",
-      },
       colors: {
         background: "hsl(var(--background-hsl))",
         foreground: "hsl(var(--foreground-hsl))",
@@ -33,12 +21,21 @@ const config: Config = {
       },
       animation: {
         "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        marquee: "marquee var(--duration) linear infinite",
       },
       keyframes: {
         "border-beam": {
           "100%": {
             "offset-distance": "100%",
           },
+        },
+        "logo-carousel": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - 2rem))" },
+        },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
         },
       },
     },
